9f8c291518b9e4e5a21b2ceba88b7cb443bdda61
lisellelogan <liselle.logan@yahoo.com>
2021-02-23 15:24:06 -0500

    create migration for recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_201850) do
+ActiveRecord::Schema.define(version: 2021_02_23_202256) do
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.integer "cook_time"
+    t.integer "servings"
+    t.text "ingredients"
+    t.text "directions"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
