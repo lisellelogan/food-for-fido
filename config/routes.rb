637373ec6f17d04d86b59735fe50391be6c88ce9
lisellelogan <liselle.logan@yahoo.com>
2021-02-25 14:24:23 -0500

    change users route to only new and create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create]
   resources :comments
   resources :recipes
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
