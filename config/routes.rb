29621d6fcf30b782f6b6180f92978415846e1bf6
lisellelogan <liselle.logan@yahoo.com>
2021-02-25 18:09:37 -0500

    create routes to signup, login, logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   root to: 'static#home'
   resources :users, only: [:new, :create, :show]
-  resources :comments
   resources :recipes
+  resources :comments
+
+  get '/login' to: 'sessions#new'
+  post '/login' to: 'sessions#create'
+  delete '/logout' to: 'sessions#destroy'
+
+  get '/signup' to: 'users#new'
+  post '/signup' to: 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
