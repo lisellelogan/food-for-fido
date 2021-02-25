9dcc7f8e6ce95516583bd4663a512fd543d42760
lisellelogan <liselle.logan@yahoo.com>
2021-02-25 18:11:14 -0500

    add ',' to sessions route

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   resources :recipes
   resources :comments
 
-  get '/login' to: 'sessions#new'
-  post '/login' to: 'sessions#create'
-  delete '/logout' to: 'sessions#destroy'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
-  get '/signup' to: 'users#new'
-  post '/signup' to: 'users#create'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
