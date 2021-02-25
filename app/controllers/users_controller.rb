ca235f690ef546757ac9f3d263443f2cf63f209e
lisellelogan <liselle.logan@yahoo.com>
2021-02-25 15:22:53 -0500

    add to create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,12 @@ class UsersController < ApplicationController
     end
 
     def create
-        user = User.create(user_params)
+        user = User.new(user_params)
+        if user.save
+            redirect_to '/'
+        else  
+            render :new
+        end
     end
 
     private
