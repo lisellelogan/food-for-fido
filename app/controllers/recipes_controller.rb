086bce5079e08f78ea65fea64dcaec563eec4f61
lisellelogan <liselle.logan@yahoo.com>
2021-02-25 10:23:27 -0500

    add strong params

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,4 +15,16 @@ class RecipesController < ApplicationController
     def create
     end
 
+    private
+
+    def recipe_params
+        params.require(:recipe).permit(
+            :name,
+            :cook_time,
+            :servings,
+            :ingredients,
+            :directions
+        )
+    end
+
 end
