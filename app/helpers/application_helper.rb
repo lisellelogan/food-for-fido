a9813be65afacbb9869e521fcfb0fe3eca51e2c8
lisellelogan <liselle.logan@yahoo.com>
2021-02-25 20:25:00 -0500

    create helper for sessions controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+    
+    def logged_in?
+        !!current_user
+    end
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+    def redirect_if_not_authorized
+        if !logged_in?
+            #login path
+        end
+    end
+    
+
 end
