da2044e163c8ea036e7c6b990ba3023ab610082f
lisellelogan <liselle.logan@yahoo.com>
2021-02-25 21:00:55 -0500

    change instance to local variable and create new action in sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,15 @@
 class SessionsController < ApplicationController
+
     def new
+        if logged_in?
+            redirect_to user_path(current_user)
+        else 
+            @user = User.new
+        end
     end
-    
+
+    def create 
+        
+    end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,10 +5,10 @@ module ApplicationHelper
     end
 
     def current_user
-        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+        current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 
-    def redirect_if_not_authorized
+    def verify_user
         # if !logged_in?
         #     redirect_to login_path
         # end
