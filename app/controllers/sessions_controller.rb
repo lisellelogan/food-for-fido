91169e7533c6dd801c4ce9d568bcf55c1e2e9a6d
lisellelogan <liselle.logan@yahoo.com>
2021-02-25 21:46:49 -0500

    create partial to display flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,6 @@ class SessionsController < ApplicationController
     def new
         if logged_in?
             redirect_to user_path(current_user)
-        else 
-            user = User.new
         end
     end
 
