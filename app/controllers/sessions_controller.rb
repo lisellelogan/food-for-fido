840e677787135e2d5f231a9fa04c89472bffa05f
lisellelogan <liselle.logan@yahoo.com>
2021-02-25 21:04:36 -0500

    add create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,19 @@ class SessionsController < ApplicationController
         if logged_in?
             redirect_to user_path(current_user)
         else 
-            @user = User.new
+            user = User.new
         end
     end
 
     def create 
-        
+        user = User.find_by(id: session[:user_id])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else  
+            flash[:message] = "Invalid credentials. Please sign up or try again."
+            redirect_to login_path
+        end
     end
 
 end
