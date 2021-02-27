c27bf7ac9577ba88bbf39ee710e3b4722bafbac7
lisellelogan <liselle.logan@yahoo.com>
2021-02-27 14:17:46 -0500

    add nested route logic to new and create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,13 @@
 class CommentsController < ApplicationController
     
     def new
-        @comment = Comment.new
+        #is this a nested route?
+        if params[:recipe_id] && @recipe = Recipe.find_by_id(params[:brand_id])
+            @comment = @recipe.comments.build 
+        else  
+            @comment = Comment.new
+            @comment.build_recipe
+        end
     end
 
     def create 
