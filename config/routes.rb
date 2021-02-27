827ce14c70a36eaaaafaf79621392a1a44423e0b
lisellelogan <liselle.logan@yahoo.com>
2021-02-27 16:52:56 -0500

    rearrange order of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   root to: 'static#home'
-  resources :users, only: [:show]
   resources :comments
   
-  resources :recipes do 
-    resources :comments, only: [:index, :new, :create]
-  end
-
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  resources :users, only: [:show]
+
+  resources :recipes do 
+    resources :comments, only: [:index, :new, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
