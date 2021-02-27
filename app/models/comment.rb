509bd4e19b5d0e41177c62c8b79f75e70fdcf606
lisellelogan <liselle.logan@yahoo.com>
2021-02-27 11:43:31 -0500

    create show action and view

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :recipe
 
-    validates :ratings, :content, presence: true
+    validates :rating, :content, presence: true
     validates :rating, numericality: { only_integer: true }
     validates :rating, inclusion: {in: 1..5}
 end
