57b05142433bdf76a9c007785d00ab1f9d100491
lisellelogan <liselle.logan@yahoo.com>
2021-02-27 16:02:37 -0500

    create helper to only show a recipe's comments

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,9 @@
 module CommentsHelper
+    def display_header(recipe)
+        if recipe
+            tag.h1("#{recipe.name}'s Comments")
+        else
+            tag.h1("All Comments")
+        end
+    end
 end
