73f77008ffc4e9a1266436464b532b71d5476673
lisellelogan <liselle.logan@yahoo.com>
2021-02-27 11:27:46 -0500

    create 'create' action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,22 @@ class CommentsController < ApplicationController
         @comment = Comment.new
     end
 
+    def create 
+        @comment = Comment.new(comment_params)
+        if @comment.save 
+            redirect_to comment_path(@comment)
+        else  
+            render :new #so that we can see our errors
+        end
+    end
+
+    private 
+
+    def comment_params
+        params.require(:comment).permit(
+            :rating,
+            :content
+        )
+    end
+
 end
