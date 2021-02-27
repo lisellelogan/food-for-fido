c2a6fe20150460cf7d0ddad51e322d48175696b7
lisellelogan <liselle.logan@yahoo.com>
2021-02-27 17:04:03 -0500

    fix user login by updated create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 class SessionsController < ApplicationController
 
     def new
-        if logged_in?
-            redirect_to user_path(current_user)
-        end
+        # if logged_in?
+        #     redirect_to user_path(current_user)
+        # end
     end
 
     def create 
-        user = User.find_by(id: session[:user_id])
+        user = User.find_by(email: params[:email])
         if user && user.authenticate(params[:password])
             session[:user_id] = user.id
             redirect_to user_path(user)
