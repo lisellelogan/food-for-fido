8e797c9c84f47ad97fae03ed73e74c732a701449
lisellelogan <liselle.logan@yahoo.com>
2021-02-27 23:48:43 -0500

    add validation to check if user is logged in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,17 +7,13 @@ module ApplicationHelper
     def logged_in?
         !!current_user
     end
-    
-    def admin?
-        !!current_user.admin
-    end
 
     def redirect_if_not_logged_in
         redirect_to login_path unless logged_in?
     end
 
     def redirect_if_not_admin
-        if !admin?
+        if !logged_in? && current_user.admin == false
             flash[:message] = "User restriction. If you are an admin, please log in."
             redirect_to login_path
         end
