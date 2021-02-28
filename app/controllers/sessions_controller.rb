5157fcc2f5437f592660c10a76e4d4852def071a
lisellelogan <liselle.logan@yahoo.com>
2021-02-28 00:26:57 -0500

    change from render to redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
             redirect_to user_path(user)
         else  
             flash[:message] = "Invalid credentials. Please sign up or try again."
-            render :new
+            redirect_to login_path
         end
     end
 
