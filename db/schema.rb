22f1955d2e84278fb9e3be5cae5e8f0adb5c45aa
lisellelogan <liselle.logan@yahoo.com>
2021-02-27 22:40:28 -0500

    add admin to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_202555) do
+ActiveRecord::Schema.define(version: 2021_02_28_033759) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "rating"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_02_23_202555) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
   end
 
 end
