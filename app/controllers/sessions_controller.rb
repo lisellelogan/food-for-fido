7b663e8ab423f2c215900619596215f5a64edf2f
lisellelogan <liselle.logan@yahoo.com>
2021-02-28 12:35:55 -0500

    create conditional for omniauth login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,6 @@ class SessionsController < ApplicationController
         end
     end
 
-    def omniauth 
-        binding.pry
-    end
-
     def create 
         user = User.find_by(email: params[:email])
         if user && user.authenticate(params[:password])
@@ -24,6 +20,29 @@ class SessionsController < ApplicationController
     def destroy
         session.delete(:user_id)
         redirect_to root_path
+    end
+
+    def omniauth 
+        user = User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
+            u.name = auth['info']['name']
+            u.email = auth['info']['email']
+            u.email_confirmation = auth['info']['email']
+            u.password = SecureRandom.hex(16) #assigns random generated password to go around password presence validation
+        end
+
+        if user.valid?
+            redirect_to signup_path
+        else 
+            flash[:message] = user.errors.full_messages.join("")
+            redirect_to root_path
+        end
+
+    end
+
+    private 
+
+    def auth 
+        request.env['omniauth.auth']
     end
 
 end
