e4a5ba48d3ee1e51e08abc9e4313ee42ce9f9135
lisellelogan <liselle.logan@yahoo.com>
2021-02-27 20:04:02 -0500

    rearrange routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   root to: 'static#home'
   resources :comments
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: "user"
   
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
-  resources :users, only: [:show]
 
   resources :recipes do 
     resources :comments, only: [:index, :new, :create]
