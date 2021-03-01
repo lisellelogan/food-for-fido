c5b1d9447018696f456f312bce7ae2403a30f428
lisellelogan <liselle.logan@yahoo.com>
2021-02-28 21:57:35 -0500

    readjust info comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'static#home'
 
   get '/signup', to: 'users#new'
@@ -24,5 +25,4 @@ Rails.application.routes.draw do
   end
 
   resources :comments
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
